a226aa07688e09c9511b1ad414939aa7f98f111a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-18 12:15:12 +0200

    v1.101

--- 1.5/Source/VAspirE/Options/VanillaAspirationsExpanded_SettingsController.cs
+++ 1.5/Source/VAspirE/Options/VanillaAspirationsExpanded_SettingsController.cs
@@ -20,6 +20,8 @@ namespace VAspirE
         public VanillaAspirationsExpanded_Mod(ModContentPack content) : base(content)
         {
             settings = GetSettings<VanillaAspirationsExpanded_Settings>();
+
+           
         }
 
         public override string SettingsCategory()
@@ -37,20 +39,7 @@ namespace VAspirE
         {
             base.DoSettingsWindowContents(inRect);
 
-            List<AspirationDef> toggleableslist = DefDatabase<AspirationDef>.AllDefsListForReading.ToList();
-
-            if (settings.aspirationStates == null) settings.aspirationStates = new Dictionary<string, bool>();
-
-            foreach (AspirationDef toggleabledef in toggleableslist)
-            {
-
-                if (!settings.aspirationStates.ContainsKey(toggleabledef.defName) && DefDatabase<AspirationDef>.GetNamedSilentFail(toggleabledef.defName) != null)
-                    {
-                        settings.aspirationStates[toggleabledef.defName] = true;
-                    }
-                
-
-            }
+          
 
             settings.DoWindowContents(inRect);
 
--- 1.5/Source/VAspirE/StaticCollections/StaticCollectionsClass.cs
+++ 1.5/Source/VAspirE/StaticCollections/StaticCollectionsClass.cs
@@ -16,8 +16,25 @@ namespace VAspirE
         public static Dictionary<Pawn, Pawn> pawns_nuzzled = new Dictionary<Pawn, Pawn>();
         public static Dictionary<Pawn, (RecipeDef,List<Thing>)> pawns_and_completed_recipes = new Dictionary<Pawn, (RecipeDef, List<Thing>)>();
        
+        static StaticCollectionsClass()
+        {
+            List<AspirationDef> toggleableslist = DefDatabase<AspirationDef>.AllDefsListForReading.ToList();
 
-        
+            if (VanillaAspirationsExpanded_Mod.settings.aspirationStates == null) VanillaAspirationsExpanded_Mod.settings.aspirationStates = new Dictionary<string, bool>();
+
+            foreach (AspirationDef toggleabledef in toggleableslist)
+            {
+
+                if (!VanillaAspirationsExpanded_Mod.settings.aspirationStates.ContainsKey(toggleabledef.defName) && DefDatabase<AspirationDef>.GetNamedSilentFail(toggleabledef.defName) != null)
+                {
+                    VanillaAspirationsExpanded_Mod.settings.aspirationStates[toggleabledef.defName] = true;
+                }
+
+
+            }
+
+        }
+
 
 
     }
