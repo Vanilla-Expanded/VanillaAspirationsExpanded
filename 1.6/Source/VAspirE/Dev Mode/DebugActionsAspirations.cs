698497a1cf8ccb08155e07ab6807967f6b12f3f3
SokyranTheDragon <exploder1531@gmail.com>
2025-07-29 20:26:22 +0200

    Extra/improved dev mode actions
    - Add aspiration dev mode option now has an option for adding a random aspiration
    - Added a dev mode option to fulfil an aspiration

--- 1.6/Source/VAspirE/Dev Mode/DebugActionsAspirations.cs
+++ 1.6/Source/VAspirE/Dev Mode/DebugActionsAspirations.cs
@@ -24,10 +24,29 @@ public static class DebugActionsAspirations
     [DebugAction("Pawns")]
     [UsedImplicitly]
     public static List<DebugActionNode> AddAspiration()
-    {     
-        List<DebugActionNode> list = new List<DebugActionNode>();
+    {
+        List<DebugActionNode> list =
+        [
+            new("*Random aspiration", DebugActionType.ToolMapForPawns)
+            {
+                pawnAction = delegate(Pawn p)
+                {
+                    var fulfilment = p.needs?.Fulfillment();
+                    if (fulfilment == null)
+                        return;
+
+                    if (DefDatabase<AspirationDef>.AllDefs
+                        .Where(aspirationDef => aspirationDef.Worker.ValidOn(p) && !fulfilment.Aspirations.Contains(aspirationDef) && VanillaAspirationsExpanded_Mod.settings.aspirationStates[aspirationDef.defName])
+                        .TryRandomElement(out var aspiration))
+                    {
+                        fulfilment.DebugAddAspiration(aspiration);
+                        DebugActionsUtility.DustPuffFrom(p);
+                    }
+                },
+            }
+        ];
         foreach (AspirationDef aspiration in DefDatabase<AspirationDef>.AllDefs)
-        {              
+        {
             list.Add(new DebugActionNode(aspiration.LabelCap, DebugActionType.ToolMapForPawns)
             {
                 pawnAction = delegate (Pawn p)
@@ -38,5 +57,21 @@ public static class DebugActionsAspirations
             });
         }
         return list;
+    }
+
+    [DebugAction("Pawns", actionType = DebugActionType.ToolMapForPawns)]
+    [UsedImplicitly]
+    public static void FulfilAspiration(Pawn p)
+    {
+        DebugActionsUtility.DustPuffFrom(p);
+        var fulfillment = p.needs?.Fulfillment();
+        if (fulfillment == null) return;
+        if (fulfillment.Aspirations.Count <= fulfillment.NumCompleted) return;
+        Find.WindowStack.Add(new Dialog_DebugOptionListLister(Gen.YieldSingle(
+                new DebugMenuOption("*Random", DebugMenuOptionMode.Action, () => fulfillment.Complete(fulfillment.Aspirations.Where(aspir => !fulfillment.IsComplete(aspir)).RandomElement())))
+            .Concat(fulfillment.Aspirations
+            .Where(aspir => !fulfillment.IsComplete(aspir))
+            .Select(aspir =>
+                new DebugMenuOption(aspir.LabelCap, DebugMenuOptionMode.Action, () => fulfillment.Complete(aspir))))));
     }
 }
